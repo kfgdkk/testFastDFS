477a16e8fc6354bc81e922386fd14548b97645af
miki <kfgdkk@163.com>
2019-04-21 11:27:08 +0800

    Initial commit

---
+++ src/FastClientTest.java
@@ -0,0 +1,15 @@
+import org.csource.fastdfs.*;
+
+public class FastClientTest {
+    public static void main(String[] args) throws Exception{
+        ClientGlobal.init("E:/client.conf");
+        TrackerClient trackerClient = new TrackerClient();
+        TrackerServer trackerServer = trackerClient.getConnection();
+        StorageServer storageServer = null;
+        StorageClient storageClient = new StorageClient(trackerServer,storageServer);
+        String[] strings = storageClient.upload_file("E:/1.jpg","jpg",null);
+        for (String string : strings){
+            System.out.println(string);
+        }
+    }
+}
