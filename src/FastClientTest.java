2d577d0d04cbeda9e34a0108937bd8b40bded56c
miki <kfgdkk@163.com>
2019-04-21 11:47:14 +0800

    第4次啊啊啊啊

--- src/FastClientTest.java
+++ src/FastClientTest.java
@@ -2,9 +2,7 @@ import org.csource.fastdfs.*;
 
 public class FastClientTest {
     public static void main(String[] args) throws Exception{
-        //注释啊啊啊啊啊
         ClientGlobal.init("E:/client.conf");
-        //为什么啊
         TrackerClient trackerClient = new TrackerClient();
         TrackerServer trackerServer = trackerClient.getConnection();
         StorageServer storageServer = null;
